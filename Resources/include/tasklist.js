f6c29d59dbde9b63ef98d95afe7b18fb86bcd468
myzac <t.m.msact@gmail.com>
2012-11-22 22:24:35 +0900

    modified to distinguish 'click' and 'longpress' added operation of database modified the form in which data are returned

--- Resources/include/tasklist.js
+++ Resources/include/tasklist.js
@@ -6,8 +6,10 @@
         // create win
         var taskListWin = Titanium.UI.createWindow({
             title:'タスク',
-            backgroundColor:'#fff'
+            backgroundColor:'#fff',
+            layout: 'absolute'
         });
+        
         // create tab
         var tab = Titanium.UI.createTab({
             icon:'KS_nav_ui.png',
@@ -26,48 +28,37 @@
             tab.open(addTaskWindow);
         });
         
-        // mock object of rows (for test run)
-        function Rows(num) {
-        	this.limit = num;
-        	this.__index = 0;
-        	this.name;
-        	this.deadline;
-        	this.importance;
-        	this.memo;
-        }
-        Rows.prototype.__iterator__ = function() {
-        	return this;
-        }
-        Rows.prototype.next = function() {
-        	return this.__index++;
-        }
-        Rows.prototype.isValidRow = function() {
-        	this.name = 'task' + this.__index;
-        	this.deadline = Math.floor(Math.random() * 500);
-        	this.importance = Math.floor(Math.random() * 3) + 1;
-        	return (this.__index >= this.limit)? false : true;
-        }
-        Rows.prototype.close = function() {
-        	
-        }
-        Rows.prototype.field = function(num) {
-        	return this.__index;
+        // sample object of records (for test run)
+        var deadline = new Array(10,30,50,100,200);
+        var importance = new Array(3,1,2,2,3);
+        function genRecords(num) {
+        	var records = Array(0);
+        	for (var i = 0; i < num; i++) {
+        		var record = {};
+        		record.id = i;
+        		record.name = 'task' + i;
+        		record.deadline = deadline[i];
+        		record.importance = importance[i];
+        		records.push(record);
+        	}
+        	return records;
         }
         
         // get tasks from DB
-        // var con = Titanium.Database.open('task');
-        // var rows = con.execute('SELECT * FROM task');
-        var rows = new Rows(5);	// for test run
+        var db = TaskDB();
+        // var records = db.fetchToList(0);
+        var records = new genRecords(5);	// for test run
         
         // draw tasks
-		while (rows.isValidRow()) {
+		for (var i = 0; i < records.length; i++) {
+			var record = records[i];
 		 	var imageView = Titanium.UI.createImageView({
-		 		id: rows.field(0),
-		 		name: rows.name,
+		 		id: record.id,
+		 		name: record.name,
 				image: 'circle_blue.png',
-				width: rows.importance * 50,	//TODO arrange tasks not to duplicate
-				top: rows.deadline - 200,
-				left: Math.floor(Math.random() * 200),
+				width: record.importance * 50 + 'dp',	//TODO arrange tasks not to duplicate
+				top: record.deadline - Titanium.Platform.displayCaps.platformHeight / 2 + 'dp',
+				left: Titanium.Platform.displayCaps.platformWidth * Math.random() * 0.8 + 'dp',
 				opacity: 0.8
 			});
 			imageView.add(Titanium.UI.createLabel({
@@ -77,36 +68,38 @@
 				touchEnabled: false
 			}));
 			taskListWin.add(imageView);
-			
-			//TODO identification : click or long-press
-       		// remove when long-pressed
-       		imageView.addEventListener('longpress', function(e){
+       		
+       		var touched = false;
+       		imageView.addEventListener('touchstart', function(e){
+       			touched = true;
        			var img = e.source;
        			var confirmAlert = 
        				Titanium.UI.createAlertDialog({
-       					title: 'Remove ' + img.name,
-       					message: 'Sure?',
-       					buttonNames: ['Yes', 'No'],
+       					title: 'タスク"' + img.name + '"を削除します．',
+       					message: 'よろしいですか?',
+       					buttonNames: ['はい', 'いいえ'],
        					cancel: 1
        				});
        			confirmAlert.addEventListener('click', function(e) {
 			   		switch (e.index) {
-			   			case 0: taskListWin.remove(img); break;	//TODO add operation of removal from DB
+			   			case 0: taskListWin.remove(img); db.deleteTask(img.id); break;
 			      		case 1: break;
 			 		}
 				});
-            	confirmAlert.show();
+				setTimeout(function() {
+					if (touched) confirmAlert.show();
+				}, 1000);
        		});
        		
 			// // create detail window
-			imageView.addEventListener('click', function(e){
+			imageView.addEventListener('touchend', function(e) {
+				touched = false;
             	var taskDetailWindow = app.taskdetail.createWindow(e.source.id);
             	tab.open(taskDetailWindow);
        		});
        		
-			rows.next();
+       		imageView.addEventListener('touchmove', function(e) { touched = false });
 		}
-		rows.close();
         
         // set button
         taskListWin.setRightNavButton(button);
